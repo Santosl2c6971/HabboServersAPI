a934d264bd0baed8fbc8eb16a5436c6579bc90bd
Santosl2c6971 <63326100+Santosl2c6971@users.noreply.github.com>
2020-04-07 22:45:12 -0300

    Arquivo de configuração

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+$_CONFIG['apiURL'] = 'http://habboservers.net/'; // Dominio do Habbo Servers
+$_CONFIG['timeout'] = 2; // Caso esteja offline quanto tempo para encontrar o Habbo Servers
+$_CONFIG['slug'] = 'habbo'; // Definido na hora de cadastrar o hotel
