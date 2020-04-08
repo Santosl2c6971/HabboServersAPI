c1bdebea1e67be42e81d87dbed9d33c2636970b9
Santosl2c6971 <63326100+Santosl2c6971@users.noreply.github.com>
2020-04-07 22:41:47 -0300

    Arquivo de configurção

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+$_CONFIG['apiURL'] = 'http://localhost/sites/habboretros/api/'; // Dominio do Habbo Servers
+$_CONFIG['timeout'] = 2; // Caso esteja offline quanto tempo para encontrar o Habbo Servers
+$_CONFIG['slug'] = 'habblive'; // Definido na hora de cadastrar o hotel
